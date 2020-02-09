634c118988c27a9ffbb4f1e4a919af177563b13f
LucasNrY <lucasdanery@usp.br>
2020-02-09 20:28:33 -0300

    [FIX] Fixed bug where the explorer was able to traverse diagonally

--- EP/structures/Graph.java
+++ EP/structures/Graph.java
@@ -30,14 +30,25 @@ public class Graph {
         for(HashMap.Entry<Vertice, LinkedList<Vertice>> connection: this.connections.entrySet()) {
             Vertice v = connection.getKey();
             LinkedList<Vertice> vList = connection.getValue();
+            Vertice upV = this.fetch(v.row - 1, v.col);
+            Vertice downV = this.fetch(v.row + 1, v.col);
+            Vertice leftV = this.fetch(v.row, v.col - 1);
+            Vertice rightV = this.fetch(v.row, v.col + 1);
 
-            for (int i = (v.row - 1); i <= (v.row + 1); i++) {
-                for (int j = (v.col - 1); j <= (v.col + 1); j++) {
-                    Vertice nextV = this.fetch(i, j);
-                    if(nextV != null && !nextV.equals(v)) {
-                        vList.add(nextV);
-                    }
-                }
+            if(leftV != null && !leftV.equals(v)) {
+                vList.add(leftV);
+            }
+
+            if(rightV != null && !rightV.equals(v)) {
+                vList.add(rightV);
+            }
+
+            if(upV != null && !upV.equals(v)) {
+                vList.add(upV);
+            }
+
+            if(downV != null && !downV.equals(v)) {
+                vList.add(downV);
             }
         }
     }
@@ -58,20 +69,15 @@ public class Graph {
         Vertice origin = this.fetch(startCoord.get("row"), startCoord.get("col"));
         Vertice dest = this.fetch(endCoord.get("row"), endCoord.get("col"));
 
-        // Mark the current node as visited and enqueue it 
         origin.was_visited = true;
         queue.add(origin);
 
         while (queue.size() != 0) {
-            // Dequeue a vertex from queue and print it 
             origin = queue.poll();
             explorer.Path.add(origin);
             explorer.collectItems(origin.items);
             explorer.addTime();
   
-            // Get all adjacent vertices of the dequeued vertex s 
-            // If a adjacent has not been visited, then mark it 
-            // visited and enqueue it 
             ListIterator<Vertice> i = this.connections.get(origin).listIterator();
             while (i.hasNext()) {
                 Vertice n = i.next();
